485d77fd01fd1641853336565b4603968248b850
Nasrin <nasrin.imanpour@gmail.com>
2025-07-21 12:56:14 -0400

    updta  setup.py

--- setup.py
+++ setup.py
@@ -9,13 +9,11 @@ setup(
     url="https://github.com/nasrinimanpour/hate-token-localizer",
     packages=find_packages(include=["src", "src.*"]),
     install_requires=[
-        "torch==1.7.1",
         "clip-by-openai",
         "taming-transformers",
         "omegaconf",
         "Pillow",
         "numpy",
-        "torchvision==0.8.2",
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
